a979ff8d448293f18b21fbeeecd9fe7cc3f56f69
Aaron Janse <gitduino@gmail.com>
2017-11-09 15:41:07 -0800

    add expression support to repl

--- compile/__main__.py
+++ compile/__main__.py
@@ -4,17 +4,21 @@ from parser import Parser
 from ast_printer import AstPrinter
 from interpreter import Interpreter
 from error import *
+import stmt
 
 
 class Main(object):
     def __init__(self):
         self.interpreter = Interpreter()
 
-    def run(self, source_code, ignore_errors=False):
+    def run(self, source_code, ignore_errors=False, repl=False):
         tokens = Scanner(source_code).scanTokens()
 
         parser = Parser(tokens)
         statements = parser.parse()
+
+        if repl and len(statements) == 1 and isinstance(statements[0], stmt.StmtExpression):
+            statements[0] = stmt.StmtPrint(statements[0].expression)
 
         if ErrorHandler.errored:
             if ignore_errors:
@@ -38,4 +42,4 @@ if len(sys.argv) > 1:
 else:
     while True:
         line = input('> ')
-        main.run(line, ignore_errors=True)
+        main.run(line, ignore_errors=True, repl=True)
